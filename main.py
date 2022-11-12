1b4a350ba150a6cbba3d02fd99f8f9a144795c6a
AbbeyIT <93916892+AbbeyIT@users.noreply.github.com>
2022-11-12 21:58:13 +0800

    FEAT: change games

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
-def tic_tac_toe():
-  print("test")
+def mathGame():
+    pass
 
-def snake_game(): 
-  pass
+def trivia():
+    pass
 
-def rock_paper_scissors():    
-  pass
+def rock_paper_scissors():
+    pass
 
-def hangman_game():
-  pass
+def wordGuess():
+    pass
+
+def sudoku():
+    pass
 
 
 def main():
     while True:
-        print("1. Tic tac toe\n2. Snake Game\n3. Rock Paper Scissor\n4. Hangman Game\n5. Exit")
+        print("1. Math Game\n2. Trivia\n3. Rock Paper Scissor\n4. Word Guessing Game\n5. Sudoku\n6. Exit")
         choice = input("Enter your choice: ")
         if choice == "1":
-            tic_tac_toe()
+            mathGame()
         elif choice == "2":
-            snake_game()
+            trivia()
         elif choice == "3":
             rock_paper_scissors()
         elif choice == "4":
-            hangman_game()
+            wordGuess()
+        elif choice == "5":
+            sudoku()
         elif choice == "5":
             break
         else:
             print("Invalid choice")
+
+if __name__ == "__main__":
+    main()

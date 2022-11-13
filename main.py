d1c3cf6356020ff43d900c9f5160b99389eaa385
Jeni Progcrammer <jennymarinay2@gmail.com>
2022-11-13 22:57:45 +0800

    FEAT: add trivia

--- main.py
+++ main.py
@@ -2,8 +2,61 @@ def mathGame():
     pass
 
 def trivia():
-    pass
+    print("Hello, Welcome to Philippines Trivia!")
 
+    ans = input("Are you ready to play? (YES/NO): ")
+    score = 0
+    total_q = 4                
+
+    if ans.lower() == 'yes':
+            ans = input("1. Name the oldest Philippine city.: ")
+            if ans == 'cebu':
+                score += 1
+                print("Correct!")
+            else:
+                print("Incorrect")
+
+            ans = input("2. What place in the Philippines is also known as the “walled city”?: ")
+            if ans.lower() == 'intramuros':
+                score += 1
+                print("Correct!")
+            else:
+                print("Incorrect")
+
+            ans = input("3. What is the capital of the Republic of the Philippines?: ")
+            if ans.lower() == 'manila':
+                score += 1
+                print("Correct!")
+            else:
+                print("Incorrect")
+
+            ans = input("4. Philippines is located in which continent: ")
+            if ans.lower() == 'asia':
+                score += 1
+                print("Correct!")
+            else:
+                print("Incorrect")
+
+            ans = input("5. Philippines national food is?: ")
+            if  ans.lower() == 'lechon':
+                score += 1
+                print("Correct!")
+            else:
+                print("Incorrect")
+
+    print("Thank you for playing, you got", score, "correct answer/s!")
+    mark = (score/total_q) * 100
+
+    print("Mark:", str(mark) + '%')
+    print('Goodbye')
+
+    ans1 = input('You want to try again? (YES/NO): ')
+    if ans1.lower() == 'yes' or ans1.upper() == 'yes':
+        main()
+    else:
+        exit()
+
+        
 def rock_paper_scissors():
     pass
 

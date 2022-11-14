c109b9359e0cd44885198d791d7c9d03f4ebda97
Abbey Santos <93916892+AbbeyIT@users.noreply.github.com>
2022-11-14 09:43:22 +0800

    FIX: bugs

--- main.py
+++ main.py
@@ -232,22 +232,25 @@ def sudoku():
 
 def main():
     while True:
-        print("1. Math Game\n2. Trivia\n3. Rock Paper Scissor\n4. Word Guessing Game\n5. Sudoku\n6. Exit")
-        choice = input("Enter your choice: ")
-        if choice == "1":
-            mathGame()
-        elif choice == "2":
-            trivia()
-        elif choice == "3":
-            rock_paper_scissors()
-        elif choice == "4":
-            wordGuess()
-        elif choice == "5":
-            sudoku()
-        elif choice == "6":
-            break
-        else:
-            print("Invalid choice")
+        try:
+            print("1. Math Game\n2. Trivia\n3. Rock Paper Scissor\n4. Word Guessing Game\n5. Sudoku\n6. Exit")
+            choice = input("Enter your choice: ")
+            if choice == "1":
+                mathGame()
+            elif choice == "2":
+                trivia()
+            elif choice == "3":
+                rock_paper_scissors()
+            elif choice == "4":
+                wordGuess()
+            elif choice == "5":
+                sudoku()
+            elif choice == "6":
+                break
+            else:
+                print("Invalid choice")
+        except:
+            print(end="")
 
 if __name__ == "__main__":
     main()
